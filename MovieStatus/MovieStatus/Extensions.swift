03f351e44abb0d3798e947dca03c41fa8289bd7f
stratigo <williamicdc@gmail.com>
2017-03-25 20:37:18 -0400

    added extension code for UIColor, added UIView constraints

---
+++ MovieStatus/MovieStatus/Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Extensions.swift
+//  MovieStatus
+//
+//  Created by xszhao on 2017-03-25.
+//  Copyright © 2017 xszhao. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(_ red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+extension UIView {
+    func addConstraintsWithFormat(_ format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
